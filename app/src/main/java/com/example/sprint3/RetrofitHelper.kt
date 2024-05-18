ec8f147ce3d9fb1d9dda06483a0a7eec6c387c99
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-18 23:39:59 +0200

    api url

--- app/src/main/java/com/example/sprint3/RetrofitHelper.kt
+++ app/src/main/java/com/example/sprint3/RetrofitHelper.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://rickandmortyapi.com/api/character/")
+            .baseUrl("https://rickandmortyapi.com/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
