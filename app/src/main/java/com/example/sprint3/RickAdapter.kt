d6b2fd1aceeaf03177de323a46c97a300cee7e37
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-18 23:40:12 +0200

    adapter

--- app/src/main/java/com/example/sprint3/RickAdapter.kt
+++ app/src/main/java/com/example/sprint3/RickAdapter.kt
@@ -3,19 +3,23 @@ package com.example.sprint3
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sprint3.databinding.ItemRickBinding
+import com.squareup.picasso.Picasso
 
 class RickAdapter(val rickList: List<RickMortyModel>) : RecyclerView.Adapter<RickViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RickViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return RickViewHolder(layoutInflater.inflate(R.layout.item_rick, parent, false))
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val binding = ItemRickBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RickViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RickViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = rickList[position]
+        Picasso.get().load(item.image).into(holder.binding.ivCharacter)
+        holder.binding.tvName.text = item.name
+    }
+
+    override fun getItemCount(): Int {
+        return rickList.size
     }
 
 }
