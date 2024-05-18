a44f12b92c21b58b40ea90b35f56dd2c6041c2d5
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-18 21:07:56 +0200

    refactor

--- app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
+++ app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.sprint3
+package com.example.sprint3.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sprint3.R
 
 class DetailsFragment : Fragment() {
 
--- app/src/main/java/com/example/sprint3/view/MainActivity.kt
+++ app/src/main/java/com/example/sprint3/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.sprint3
+package com.example.sprint3.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sprint3.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
