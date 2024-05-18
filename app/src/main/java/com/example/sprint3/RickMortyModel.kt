6a72e6a4f01e45456f616d02d6f5552143698a8f
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-18 21:08:20 +0200

    view holder

--- app/src/main/java/com/example/sprint3/RickMortyModel.kt
+++ app/src/main/java/com/example/sprint3/RickMortyModel.kt
@@ -2,9 +2,6 @@ package com.example.sprint3
 
 import com.google.gson.annotations.SerializedName
 
-data class RickMortyModel (@SerializedName("name") var name:String,
-                           @SerializedName("status") var status:String,
-                           @SerializedName("species") var species:String,
-                           @SerializedName("gender") var gender:String,
-                           @SerializedName("image") var image:String) {
+data class RickMortyModel (@SerializedName("name") val name:String,
+                           @SerializedName("image") val image:String) {
 }
---
+++ app/src/main/java/com/example/sprint3/RickViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.sprint3
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RickViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+}
