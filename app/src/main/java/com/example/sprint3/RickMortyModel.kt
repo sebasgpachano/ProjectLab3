4ae58fed360c57fe588afc0de94ccc3c5ba9b582
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-19 12:22:17 +0200

    model update

--- app/src/main/java/com/example/sprint3/RickMortyModel.kt
+++ app/src/main/java/com/example/sprint3/RickMortyModel.kt
@@ -3,5 +3,8 @@ package com.example.sprint3
 import com.google.gson.annotations.SerializedName
 
 data class RickMortyModel (@SerializedName("name") val name:String,
-                           @SerializedName("image") val image:String) {
+                           @SerializedName("image") val image:String,
+                           @SerializedName("status") val status:String,
+                           @SerializedName("species") val species:String,
+                           @SerializedName("gender") val gender:String) {
 }
