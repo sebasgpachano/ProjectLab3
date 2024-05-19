7e6fac5cb681fdbcc436b6a6e2cecc7c0f768308
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-19 23:23:30 +0200

    getCharacter call

--- app/src/main/java/com/example/sprint3/RickApiClient.kt
+++ app/src/main/java/com/example/sprint3/RickApiClient.kt
@@ -2,8 +2,12 @@ package com.example.sprint3
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface RickApiClient {
     @GET("character/[1,2,3,4,5,6,7,8,9,10]")
     fun getCharacters(): Call<List<RickMortyModel>>
+
+    @GET
+    fun getCharacter(@Url url: String): Call<RickMortyModel>
 }
