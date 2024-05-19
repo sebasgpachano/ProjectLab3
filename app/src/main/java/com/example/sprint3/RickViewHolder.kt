e31430b4138714e85ddaa63b432877912662d9a0
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-19 23:23:56 +0200

    recycler view viewholder view

--- app/src/main/java/com/example/sprint3/RickViewHolder.kt
+++ app/src/main/java/com/example/sprint3/RickViewHolder.kt
@@ -3,7 +3,18 @@ package com.example.sprint3
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sprint3.databinding.ItemRickBinding
+import com.squareup.picasso.Picasso
 
-class RickViewHolder(val binding: ItemRickBinding) : RecyclerView.ViewHolder(binding.root) {
+class RickViewHolder(
+    val binding: ItemRickBinding,
+    private val listener: RickAdapter.OnItemClickListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: RickMortyModel) {
+        Picasso.get().load(item.image).into(binding.ivCharacter)
+        binding.tvName.text = item.name
+        binding.container.setOnClickListener {
+            listener.onItemClick(item.id)
+        }
+    }
 
 }
