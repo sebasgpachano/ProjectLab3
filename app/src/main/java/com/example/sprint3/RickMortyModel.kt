49c6a287b9b4cf173d567231d69ed2d652b4e55f
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-19 19:00:21 +0200

    id added to get from api

--- app/src/main/java/com/example/sprint3/RickMortyModel.kt
+++ app/src/main/java/com/example/sprint3/RickMortyModel.kt
@@ -2,7 +2,9 @@ package com.example.sprint3
 
 import com.google.gson.annotations.SerializedName
 
-data class RickMortyModel (@SerializedName("name") val name:String,
+data class RickMortyModel(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
                            @SerializedName("image") val image:String,
                            @SerializedName("status") val status:String,
                            @SerializedName("species") val species:String,
