af5812c71a9f48575526703e9b0cdae68db088ec
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-19 18:58:34 +0200

    args message test

--- app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
+++ app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.sprint3.R
+import com.example.sprint3.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args: DetailsFragmentArgs by navArgs()
+        val id = args.id
+
+        binding.tvName.text = id.toString()
+    }
 }
