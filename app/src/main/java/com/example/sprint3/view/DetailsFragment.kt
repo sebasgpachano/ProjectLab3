c12c42207dcaceedd047fbbbd2cf324ad460dbe0
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-22 03:21:39 +0200

    error management

--- app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
+++ app/src/main/java/com/example/sprint3/view/DetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -48,6 +49,12 @@ class DetailsFragment : Fragment(), View.OnClickListener {
 
         detailsViewModel.isLoading.observe(viewLifecycleOwner, Observer {
             showLoading(it)
+        })
+
+        detailsViewModel.error.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            }
         })
     }
 
--- app/src/main/java/com/example/sprint3/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/example/sprint3/viewmodel/DetailsViewModel.kt
@@ -18,6 +18,9 @@ class DetailsViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
     fun fetchItemDetails(id: Int) {
         _isLoading.value = true
         viewModelScope.launch {
@@ -27,7 +30,7 @@ class DetailsViewModel : ViewModel() {
                 val call = rickApiClient.getCharacter("character/$id")
                 _characterDetails.value = call.body()
             } catch (e: Exception) {
-                null
+                _error.value = (e.message ?: "Error desconocido")
             } finally {
                 _isLoading.value = false
             }
