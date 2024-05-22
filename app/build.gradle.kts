2d443963c58dc38498ab5b15fd6692e83702fa4f
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-05-22 20:36:52 +0200

    safe args plugin refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.safeargs)
 }
 
 android {
