f0bbb8d3210920bc66478c36bf62128b2427acc3
Sebastian <sebastiangarciapachano@gmail.com>
2024-09-10 15:53:40 +0200

    navhostfragment

--- app/src/main/java/com/example/sprint3/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/sprint3/ui/main/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.sprint3.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.example.sprint3.R
+import com.example.sprint3.databinding.ActivityMainBinding
+import com.example.sprint3.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun inflateBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun observeViewModel() = Unit
+
+    override fun createAfterInflateBindingSetupObserverViewModel(savedInstanceState: Bundle?) {
+        configNavigation()
+    }
+
+    override fun configureToolbarAndConfigScreenSections() = Unit
+
+    private fun configNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
