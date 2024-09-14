cfaae06d9e2ddb267ed4ca747dc165c671ad7fb1
Sebastian <sebastiangarciapachano@gmail.com>
2024-09-14 17:01:39 +0200

    character viewmodel refactor

--- app/src/main/java/com/example/sprint3/ui/character/CharacterViewModel.kt
+++ app/src/main/java/com/example/sprint3/ui/character/CharacterViewModel.kt
@@ -1,41 +1,43 @@
 package com.example.sprint3.ui.character
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.sprint3.data.repository.remote.backend.ApiService
-import com.example.sprint3.data.repository.remote.response.characters.RickMortyModel
-import com.example.sprint3.retrofit.RetrofitHelper
+import com.example.sprint3.data.domain.model.character.CharacterModel
+import com.example.sprint3.data.domain.usecase.GetCharacterUseCase
+import com.example.sprint3.data.repository.remote.response.BaseResponse
+import com.example.sprint3.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CharacterViewModel : ViewModel() {
+@HiltViewModel
+class CharacterViewModel @Inject constructor(private val getCharacterUseCase: GetCharacterUseCase) :
+    BaseViewModel() {
 
-    private val _rickList = MutableLiveData<List<RickMortyModel>>()
-    val rickList: LiveData<List<RickMortyModel>> get() = _rickList
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> get() = _isLoading
-
-    private val _error = MutableLiveData<String>()
-    val error: LiveData<String> get() = _error
+    private val rickListMutableStateFlow = MutableStateFlow<List<CharacterModel>>(emptyList())
+    val rickStateFlow: StateFlow<List<CharacterModel>> = rickListMutableStateFlow
 
     init {
         fetchData()
     }
 
-    fun fetchData() {
-        _isLoading.value = true
-        viewModelScope.launch {
-            try {
-                val retrofit = RetrofitHelper.getRetrofit()
-                val rickApiClient = retrofit.create(ApiService::class.java)
-                val call = rickApiClient.getCharacters()
-                _rickList.value = call.body()
-            } catch (e: Exception) {
-                _error.value = (e.message ?: "Error desconocido")
-            } finally {
-                _isLoading.value = false
+    private fun fetchData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            loadingMutableSharedFlow.emit(true)
+            getCharacterUseCase().collect {
+                when (it) {
+                    is BaseResponse.Error -> {
+                        loadingMutableSharedFlow.emit(false)
+                        errorMutableSharedFlow.emit(it.error)
+                    }
+
+                    is BaseResponse.Success -> {
+                        loadingMutableSharedFlow.emit(false)
+                        rickListMutableStateFlow.value = it.data
+                    }
+                }
             }
         }
     }
